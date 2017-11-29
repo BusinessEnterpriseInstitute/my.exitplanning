086eadc4ac9d8c12e23b9868737a281910f86d1a
Osborn Ogonyo <oogonyo@exitplanning.com>
2017-11-29 16:03:56 -0700

    Added video-gallery--page.tpl
    Added theming elements to video gallery page

---
+++ docroot/sites/all/themes/bei/templates/views-view-unformatted--video-gallery--page.tpl.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<div class="columns large-2 small-12">
+	<!-- Print custom block content -->
+	$block = module_invoke('block','block_view','31');
+	print render($block['content']);
+</div>
+<div class="columns small-12 large-10">
+<?php foreach ($rows as $id => $row): ?>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <?php print $row; ?>
+  </div>
+<?php endforeach; ?>
+</div>
