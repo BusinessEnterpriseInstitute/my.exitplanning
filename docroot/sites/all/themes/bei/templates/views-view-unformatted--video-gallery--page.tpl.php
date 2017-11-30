fd3d63fae62b38e9916b74ed3f10c4c461f88408
Osborn Ogonyo <oogonyo@exitplanning.com>
2017-11-30 09:07:39 -0700

    Updated video-gallery.tpl
    Commented out some lines in the code

--- docroot/sites/all/themes/bei/templates/views-view-unformatted--video-gallery--page.tpl.php
+++ docroot/sites/all/themes/bei/templates/views-view-unformatted--video-gallery--page.tpl.php
@@ -10,14 +10,15 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<div class="columns large-2 small-12">
+<!--div class="columns large-2 small-12">
 	<!-- Print custom block content -->
 	<?php
-		    $block = module_invoke('block', 'block_view', 31);
-		    print render($block['content']);
+		   /** $block = module_invoke('block', 'block_view', 31);
+		   * print render($block['content']);
+		   */
 	?>
 
-</div>
+</div-->
 <div class="columns small-12 large-10">
 <?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
@@ -25,10 +26,10 @@
   </div>
 <?php endforeach; ?>
 </div>
-<div>
+<!--div>
 	<?php
-	$views_block = module_invoke('views','block_view','cta-block');
-	print render($views_block);
-
+	/**$views_block = module_invoke('views','block_view','cta-block');
+	*print render($views_block);
+   */
 	?>
-</div>
+</div-->
