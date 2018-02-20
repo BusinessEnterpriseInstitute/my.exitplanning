46a1ac88043401bf07af90439f6b5a7d35cec7a9
Osborn Ogonyo <oogonyo@exitplanning.com>
2018-02-20 16:19:56 -0700

    Added IE Shims

--- docroot/sites/all/themes/bei/templates/html.tpl.php
+++ docroot/sites/all/themes/bei/templates/html.tpl.php
@@ -53,6 +53,10 @@
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+    <script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
+    <script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 </head>
