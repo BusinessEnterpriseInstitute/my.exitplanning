e4b752d21ef938165f14a3750307d9d792f6db46
Osborn Ogonyo <oogonyo@exitplanning.com>
2018-03-22 16:12:45 -0600

    Cleaned up code

--- docroot/sites/all/themes/bei/templates/views-view-unformatted--video-gallery--page.tpl.php
+++ docroot/sites/all/themes/bei/templates/views-view-unformatted--video-gallery--page.tpl.php
@@ -10,15 +10,7 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<!--div class="columns large-2 small-12">
-	<!-- Print custom block content -->
-	<?php
-		   /** $block = module_invoke('block', 'block_view', 31);
-		   * print render($block['content']);
-		   */
-	?>
 
-</div-->
 <div class="columns small-12 large-10">
 <?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
@@ -26,10 +18,3 @@
   </div>
 <?php endforeach; ?>
 </div>
-<!--div>
-	<?php
-	/**$views_block = module_invoke('views','block_view','cta-block');
-	*print render($views_block);
-   */
-	?>
-</div-->
