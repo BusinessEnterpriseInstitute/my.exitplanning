1f6ce2c04bbe8faf88d29898a24afab554e3b1fa
Osborn Ogonyo <oogonyo@exitplanning.com>
2020-01-27 08:43:03 -0700

    Added Hotjar tracking code

--- docroot/sites/all/themes/bei/templates/page.tpl.php
+++ docroot/sites/all/themes/bei/templates/page.tpl.php
@@ -251,6 +251,18 @@
         <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/6036342.js"></script>
         <!-- End of HubSpot Embed Code -->
       <!--End of Hubspot Script-->
+      <!-- Hotjar Tracking Code for www.exitplanning.com -->
+      <script>
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:1659536,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      </script>
+      <!--End of Hotjar Tracking Code -->
     <?php endif;?>
   </footer>
   <!--/.l-footer -->
