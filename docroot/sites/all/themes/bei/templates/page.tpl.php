c64ed15f8d6cf53c40824251a3424051a9becc9e
Osborn Ogonyo <oogonyo@exitplanning.com>
2020-01-27 13:15:25 -0700

    Update Hotjar

--- docroot/sites/all/themes/bei/templates/page.tpl.php
+++ docroot/sites/all/themes/bei/templates/page.tpl.php
@@ -255,7 +255,7 @@
       <script>
           (function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:1659536,hjsv:6};
+              h._hjSettings={hjid:750316,hjsv:6};
               a=o.getElementsByTagName('head')[0];
               r=o.createElement('script');r.async=1;
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
