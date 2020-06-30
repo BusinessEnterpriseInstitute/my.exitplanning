90efa7e5e3d2119165b925d4964aadc64bb6ecf1
Osborn Ogonyo <oogonyo@exitplanning.com>
2020-06-30 09:24:48 -0600

    Added Single Signon module

---
+++ docroot/sites/all/modules/single_sign_on/js/single_sign_on.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function($){
+    
+    // Register Session.
+    $.ajax({
+        url: '/ajax/single_sign_on/get_user_sso_token', 
+        method :'GET',
+        dataType: 'json', 
+        success: function(result)
+        {
+           	if (result != undefined && typeof result === 'string' && result.length)
+           	{
+                // Check for existing session.
+                if (result == 'check_session')
+                {
+                    _oneall.push(['single_sign_on', 'do_check_for_sso_session', window.location.href, true]);                
+                } 
+                // Refresh current session.
+                else
+                {
+                    if (result != 'no_token_found')
+                    {
+                        _oneall.push(['single_sign_on', 'do_register_sso_session', result]);
+                    }
+                }
+        	}
+        }
+    });
+    
+    // Retrieve User Notices.
+    $.ajax({
+        url: '/ajax/single_sign_on/get_user_notice', 
+        method :'GET',
+        dataType: 'json', 
+        success: function(result)
+        {
+            if (result != undefined && typeof result === 'string' && result.length)
+            {
+                $('#single_sign_on_notice_container').html(result);
+            }
+        }
+    });
+});
