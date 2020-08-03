5bae671f5b458ad94ecc69d9429c86a2e5b87523
Osborn Ogonyo <oogonyo@exitplanning.com>
2020-08-03 09:40:55 -0600

    Reinstalled miniorange module

---
+++ docroot/sites/all/modules/miniorange_saml/includes/constants.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Contains constants class.
+ */
+
+/**
+ * @file
+ * This class represents constants used throughout project.
+ */
+class MiniorangeSAMLConstants
+{
+
+    const BASE_URL = 'https://login.xecurify.com';
+    const DEFAULT_PUBLIC_CERT_NAME='sp-certificate.crt';
+    const CUSTOM_PUBLIC_CERT_NAME='Custom_Public_Certificate.crt';
+    const DEFAULT_PRIVATE_KEY_NAME='sp-key.key';
+    const CUSTOM_PRIVATE_KEY_NAME='Custom_Private_Certificate.key';
+
+}
