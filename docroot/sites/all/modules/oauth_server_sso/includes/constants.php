cfff38aa87d2272a9a488c052467cf6efcbb64b2
Osborn Ogonyo <oogonyo@exitplanning.com>
2020-08-03 07:11:59 -0600

    Added Miniorange modules to disable

---
+++ docroot/sites/all/modules/oauth_server_sso/includes/constants.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Contains constants class.
+ */
+
+/**
+ * @file
+ * This class represents constants used throughout project.
+ */
+class MiniorangeOAuthServerConstants {
+
+  const BASE_URL = 'https://auth.miniorange.com';
+
+}
