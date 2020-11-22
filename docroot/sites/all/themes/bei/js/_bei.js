f5688f4fc7192cdbf502316bac3595ff3c2925fc
Osborn Ogonyo <oogonyo@exitplanning.com>
2020-11-22 11:33:58 -0700

    Removed HS bucket scripts

--- docroot/sites/all/themes/bei/js/_bei.js
+++ docroot/sites/all/themes/bei/js/_bei.js
@@ -50,20 +50,7 @@
         }
       });
 
-      // Hubspot Bucket Credits
-      var _hsq = window._hsq = window._hsq || [];
-      _hsq.push(['setPath', '/blog']);
-      _hsq.push(['trackPageView']);
-      _hsq.push(['setPath', '/podcast']);
-      _hsq.push(['trackPageView']);
-      _hsq.push(['setPath', '/event']);
-      _hsq.push(['trackPageView']);
-      _hsq.push(['setPath', '/solutions']);
-      _hsq.push(['trackPageView']);
-      _hsq.push(['setPath', '/membership']);
-      _hsq.push(['trackPageView']);
-      _hsq.push(['setPath', '/contact-us']);
-      _hsq.push(['trackPageView']);
+      
 /**
       $(function(c) {
       $('.block-webform-client-block-181086').on('click', ".alert-close", function(c){
